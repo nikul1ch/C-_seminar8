56672c3ee0efab900d43c6f27407986bb534bdf2
Pavel Nikulin <nikulin@netkam.ru>
2022-09-19 13:01:04 +0300

    Программа, которая будет находить произведение двух матриц.

--- seminar8.3/Program.cs
+++ seminar8.3/Program.cs
@@ -1,12 +1,4 @@
-﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
-// Например, даны 2 матрицы:
-// 2 4 | 3 4
-// 3 2 | 3 3
-// Результирующая матрица будет:
-// 18 20
-// 15 18
-
-int[,] CreateMatrix(int rowCount, int columnCount)
+﻿int[,] CreateMatrix(int rowCount, int columnCount)
 {
     int[,] matrix = new int[rowCount, columnCount];
     for (int row = 0; row < rowCount; row++)
